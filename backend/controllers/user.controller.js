985a1a79b757eb6bdef761420d6f322d3a61d921
Harsh Bosamiya <202201243@daiict.ac.in>
2024-11-06 22:48:16 +0530

    Implemented remaining routes and added the required code in the respective files

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -63,6 +63,16 @@ export const login = async (req, res) => {
 
         const token = await jwt.sign({ userId: user._id }, process.env.SECRET_KEY, { expiresIn: '1d' });
 
+        const populatedPosts = await Promise.all(
+            user.posts.map( async (postId) => {
+                const post = await Post.findById(postId);
+                if(post.author.equals(user._id)){
+                    return post;
+                }
+                return null;
+            })
+        )
+
         user = {
             _id: user._id,
             username: user.username,
