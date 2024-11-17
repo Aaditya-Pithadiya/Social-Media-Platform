a6d04c4fe572471f4cfe61fa08559995afdaefbd
Harsh Bosamiya <202201243@daiict.ac.in>
2024-11-17 15:32:07 +0530

    made some changes for deployment

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,17 @@ import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
 import messageRoute from "./routes/message.route.js";
 import {app , server} from "./socket/socket.js"
+import path from "path";
+
 
 dotenv.config({});
 // const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
+
+console.log(__dirname);
 
 app.get("/", (_,res)=>{
     return res.status(200).json({
@@ -36,6 +42,12 @@ app.use(cors(corsOptions));
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/post", postRoute);
 app.use("/api/v1/message", messageRoute);
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+app.get("*", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+})
 
 
 
